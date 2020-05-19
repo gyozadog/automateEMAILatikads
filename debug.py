e399ef3b52f4eda258a95e602e52834cf68367f8
gyozadog <64586816+gyozadog@users.noreply.github.com>
2020-05-19 10:58:45 +0700

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,6 +1,6 @@
-from send_email import extract_summary
+from send_email import extract_contacts
 import fire
 
 if __name__ == '__main__':
   # Export to Fire
-  fire.Fire(extract_summary)
+  fire.Fire(extract_contacts)
